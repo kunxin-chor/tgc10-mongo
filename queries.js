e6a7ddee66ed6405b1d9498149360cd62a0a0c11
kunxin-chor <chorkunxin@yahoo.com>
2021-02-17 13:26:03 +0000

    Added more queries

--- queries.js
+++ queries.js
@@ -27,3 +27,80 @@ db.listingsAndReviews.find({},{
     name: 1,
     'address.country': 1
 }).pretty()
+
+// finding by criteria
+
+
+// find all listings with exactly 5 beds
+db.listingsAndReviews.find({
+    beds:5
+},{
+    name: 1,
+    beds: 1
+}).pretty()
+
+// find by a key in an embedded document
+// (aka sub-document, aka nested object)
+// find all listings in Brazil
+db.listingsAndReviews.find({
+    'address.country':'Brazil'
+},{
+    name: 1,
+    'address.country': 1    
+}).pretty()
+
+// multiple critera
+// find listings that have 5 beds
+// and 5 bedrooms
+db.listingsAndReviews.find({
+    beds: 5,
+    bedrooms: 5
+}, {
+    name: 1,
+    beds: 1,
+    bedrooms:1
+}).pretty()
+
+// find listings with 5 beds and 5 bedrooms
+// and in Brazil
+db.listingsAndReviews.find({
+    beds: 5,
+    bedrooms: 5,
+    'address.country':'Brazil'
+}, {
+    beds: 1,
+    bedrooms: 1,
+    'address.country':1,
+    name: 1
+}).pretty()
+
+// we want to find listings that have more than 3 beds
+db.listingsAndReviews.find({
+    beds: {
+        '$gt': 3
+    }
+}, {
+    name:1,
+    beds:1
+}).pretty()
+
+// find listings with less than 4 beds
+db.listingsAndReviews.find({
+    beds: {
+        '$lt': 4
+    }
+},{
+    name: 1,
+    beds: 1
+}).pretty()
+
+// find listings that have 4 to 8 beds
+db.listingsAndReviews.find({
+    beds: {
+        '$gte': 4,
+        '$lte': 8
+    }
+}, {
+    name: 1,
+    beds: 1
+})
